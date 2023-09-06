3a6dd6ef06309e119ecee72b19d0f1205c4f6f25
Illei Adrian <illei.adrian@yahoo.com>
2023-09-06 10:57:33 +0200

    Add project files.

---
+++ lab1ex4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lab1ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Scrieti un program care va afisa semnul unui numar citit de la tastatura
+            • Daca numarul este pozitiv, va afisa “+”
+            • Daca numarul este negativ, va afisa “-”
+            • Daca numarul este 0, va afisa “0”
+            */
+            int numar;
+            numar = int.Parse(Console.ReadLine());
+            if( numar > 0)
+            {
+                Console.WriteLine("+");
+            }
+            if (numar < 0)
+            {
+                Console.WriteLine("-");
+            }
+            if (numar == 0) Console.WriteLine("0");
+            
+        }
+    }
+}
